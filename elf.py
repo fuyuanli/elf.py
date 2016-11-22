88b568771ca7aa904a0b6c6a115958d221950ca1
FuYuan Li <fuyuanli0625@gmail.com>
2016-11-23 03:07:45 +0800

    requirement.txt

--- elf.py
+++ elf.py
@@ -43,11 +43,48 @@ class elf:
     def setPlace(self, num):
         num = int(num)
         s = self.session
+        url = "https://www.airelf.com.tw/CN/index.aspx"
         if num <= 3 or num >= 0:
-            r = s.get("https://www.airelf.com.tw/CN/index.aspx")
+            r = s.get(url)
             soup = BeautifulSoup(r.text, 'html.parser')
             thePlace = soup.find(id=num).a.string
             print(thePlace)
 
             theID = soup.find(id=str(num)+"_0").a["href"]
             self.palceID = theID.split("=")[1]
+
+    def removeTab(self, string):
+        string = string.replace("\t", "" )
+        string = string.replace("\n", "" )
+        string = string.replace("\r", "" )
+        return string
+
+    def getAddress(self):
+        s = self.session
+        url = "https://www.airelf.com.tw/CN/stationaddress.aspx?IDCompany=" + self.palceID
+        r = s.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        theAddress = soup.findAll("td")[1].string
+        theAddress = self.removeTab(theAddress)
+
+        theZIP = soup.findAll("td")[3].string
+        theZIP = self.removeTab(theZIP)
+
+        theUser = soup.findAll("td")[5].string
+        theUser = self.removeTab(theUser)
+
+        theNumber = soup.findAll("td")[7].string
+        theNumber = self.removeTab(theNumber)
+
+        thePhone = soup.findAll("td")[9].string
+        thePhone = self.removeTab(thePhone)
+
+        theData = {
+            "address":theAddress,
+            "zip":theZIP,
+            "user":theUser,
+            "number":theNumber,
+            "phone":thePhone
+        }
+        return theData
